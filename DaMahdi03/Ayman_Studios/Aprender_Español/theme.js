61dcaff844837db00ebedfc265a736113565ed0f
Mahdi Ayman <damahdi03@gmail.com>
2018-09-25 18:56:56 -0700

    Added Aprender_Español Project (Stay Tuned For More!!!)

---
+++ DaMahdi03/Ayman_Studios/Aprender_Español/theme.js
@@ -0,0 +1,9 @@
+var headTag = document.getElementsByTagName("head")[0];
+var TitillumWebFont = document.createElement("link");
+TitillumWebFont.setAttribute("href", "https://fonts.googleapis.com/css?family=Titillium+Web");
+TitillumWebFont.setAttribute("rel", "stylesheet");
+headTag.append(TitillumWebFont);
+/*var bodyTag = document.getElementsByTagName("body")[0];
+var jQueryScriptElement = document.createElement("script");
+jQueryScriptElement.setAttribute("src", "https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js");
+bodyTag.appendChild(jQueryScriptElement);*/
