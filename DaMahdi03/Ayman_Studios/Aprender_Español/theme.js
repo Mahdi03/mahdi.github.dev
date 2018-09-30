f1cf48a140dd2e95e466be581bb2cd00d89562c1
Mahdi Ayman <damahdi03@gmail.com>
2018-09-29 18:27:25 -0700

    Hid The Lesson And The Practice, That Way Things Would Be Completed In Order

--- DaMahdi03/Ayman_Studios/Aprender_Español/theme.js
+++ DaMahdi03/Ayman_Studios/Aprender_Español/theme.js
@@ -1,16 +1,7 @@
-var headTag = document.getElementsByTagName("head")[0];
-var TitillumWebFont = document.createElement("link");
-TitillumWebFont.setAttribute("href", "https://fonts.googleapis.com/css?family=Titillium+Web");
-TitillumWebFont.setAttribute("rel", "stylesheet");
-headTag.append(TitillumWebFont);
-/*var bodyTag = document.getElementsByTagName("body")[0];
-var jQueryScriptElement = document.createElement("script");
-jQueryScriptElement.setAttribute("src", "https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js");
-bodyTag.appendChild(jQueryScriptElement);*/
 var scrollInterval = setInterval(function() {
     if ($(document).scrollTop() > ($(".lesson").outerHeight(true) + $(".intro").outerHeight(true))) {
         $(".practice").css("visibility", "visible");
         alert("If you feel ready enough, continue on to the Practice Section.");
         clearInterval(scrollInterval);
     }
-}, 1000);
+}, 500);
